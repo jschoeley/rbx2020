7030639a69f794e41dc4eae6675afa4fd9d8573f
Jonas Schöley <jschoeley@gmail.com>
2021-05-06 15:39:59 +0200

    Update List of Model Parameterizations
    - Update to be in line with new model specifications
    - use ISO week for everything

--- src/02b-parameterize_models.R
+++ src/02b-parameterize_models.R
@@ -1,5 +1,9 @@
 # Parameterize expected death models
 
+# A list specifying all the models to be tested, and their
+# parametrizations. See specify_models.R for the exact
+# implementation of the models.
+
 # Init ------------------------------------------------------------
 
 library(dplyr)
@@ -15,60 +19,66 @@ cnst <- within(cnst, {
 
 # Specifications of models to test --------------------------------
 
-# Just a big list specifying all the models to be tested,
-# and their parametrizations. See specify_models.R for the exact
-# implementation of the models.
-
 mod_para <-
   tribble(
     
-    ~model_id, ~model_class, ~model_spec,
+    ~model_id, ~model_class, ~model_para,
     
-    # ... Average -----------------------------------------------------
+    # ... AVG ---------------------------------------------------------
     
-    'AVGc5', 'avg', list(
-      stat = 'deaths', n_years = 5
-    ),
-    'AVGc3', 'avg', list(
-      stat = 'deaths', n_years = 3
-    ),
-    
-    'AVGr5', 'avg', list(
-      stat = 'mortality', n_years = 5
-    ),
-    'AVGr3', 'avg', list(
-      stat = 'mortality', n_years = 3
+    'AVRc5', 'glm', list(
+      formula = formula(
+        deaths_observed ~
+          # single coefficient for every weeks
+          as.factor(iso_week)*stratum_id
+      ),
+      family = quasipoisson(link = 'log'),
+      n_years_for_training = 5,
+      weeks_for_training = NULL
     ),
     
-    # ... Serfling ----------------------------------------------------
+    'AVRr5', 'glm', list(
+      formula = formula(
+        deaths_observed ~
+          # single coefficient for every weeks
+          as.factor(iso_week)*stratum_id +
+          offset(log(personweeks))
+      ),
+      family = quasipoisson(link = 'log'),
+      n_years_for_training = 5,
+      weeks_for_training = NULL
+    ),
+
+    # ... SRF ---------------------------------------------------------
     
+    # Euromomo style Serfling
     # https://github.com/EuroMOMOnetwork/MOMO/blob/master/R/excess.R
-    'SRFcem', 'serfling', list(
+    'SRFcem', 'glm', list(
       formula = formula(
         deaths_observed ~
           # log linear long term trend
           origin_weeks*stratum_id +
-          # seasonality
-          # full year period
-          sin(2*pi*epi_week/52)*stratum_id +
-          cos(2*pi*epi_week/52)*stratum_id
+          # seasonality full year period
+          sin(2*pi/52*iso_week)*stratum_id +
+          cos(2*pi/52*iso_week)*stratum_id
       ),
       family = quasipoisson(link = 'log'),
       weeks_for_training = c(15:26, 36:45),
       n_years_for_training = 5
     ),
-    'SRFc', 'serfling', list(
+    # Forecasting Serfling without exposures
+    'SRFc', 'glm', list(
       formula = formula(
         deaths_observed ~
           # log linear long term trend
           origin_weeks*stratum_id +
           # seasonality
           # full year period
-          sin(2*pi*epi_week/52)*stratum_id +
-          cos(2*pi*epi_week/52)*stratum_id +
+          sin(2*pi/52*iso_week)*stratum_id +
+          cos(2*pi/52*iso_week)*stratum_id +
           # half year period
-          sin(2*pi*epi_week/26)*stratum_id +
-          cos(2*pi*epi_week/26)*stratum_id +
+          sin(2*pi/26*iso_week)*stratum_id +
+          cos(2*pi/26*iso_week)*stratum_id +
           # adjustment for special weeks
           holiday3*stratum_id
       ),
@@ -76,18 +86,19 @@ mod_para <-
       weeks_for_training = NULL,
       n_years_for_training = NULL
     ),
-    'SRFr', 'serfling', list(
+    # Forecasting Serfling with exposures
+    'SRFr', 'glm', list(
       formula = formula(
         deaths_observed ~
           # log linear long term trend
           origin_weeks*stratum_id +
           # seasonality
           # full year period
-          sin(2*pi*epi_week/52)*stratum_id +
-          cos(2*pi*epi_week/52)*stratum_id +
+          sin(2*pi/52*iso_week)*stratum_id +
+          cos(2*pi/52*iso_week)*stratum_id +
           # half year period
-          sin(2*pi*epi_week/26)*stratum_id +
-          cos(2*pi*epi_week/26)*stratum_id +
+          sin(2*pi/26*iso_week)*stratum_id +
+          cos(2*pi/26*iso_week)*stratum_id +
           # adjustment for special weeks
           holiday3*stratum_id +
           # exposures
@@ -100,61 +111,44 @@ mod_para <-
 
     # ... GAM ---------------------------------------------------------
     
+    # Gam without temperature
     'GAMr', 'gam', list(
       formula = formula(
         deaths_observed ~
           # log linear long term trend
           origin_weeks*stratum_id +
           # penalized cyclic spline for seasonality
-          s(epi_week, bs = 'cp', k = 12, by = stratum_id) +
+          s(iso_week, bs = 'cp', k = 12, by = stratum_id) +
           # adjustment for special weeks
           holiday3*stratum_id +
           # exposures
           offset(log(personweeks))
       ),
-      family = quasipoisson(link = 'log'),
-      method = 'GCV.Cp'
+      family = quasipoisson(link = 'log')
     ),
+    # Gam with temperature
     'GAMrt', 'gam', list(
       formula = formula(
         deaths_observed ~
           # log linear long term trend
           origin_weeks*stratum_id +
           # penalized cyclic spline for seasonality
-          s(epi_week, bs = 'cp', k = 12, by = stratum_id) +
+          s(iso_week, bs = 'cp', k = 12, by = stratum_id) +
           # temperature effect
-          s(epi_week, bs = 'cp', k = 12, by = temperature_anomaly) +
+          s(iso_week, bs = 'cp', k = 12, by = temperature_anomaly) +
           # adjustment for special weeks
           holiday3*stratum_id +
           # exposures
           offset(log(personweeks))
       ),
-      family = quasipoisson(link = 'log'),
-      method = 'GCV.Cp'
+      family = quasipoisson(link = 'log')
     ),
     
-    # ... Time series -------------------------------------------------
+    # ... LGM ---------------------------------------------------------
     
-    # 'Seasonal exponential smoothing (AAA, 1,1,52) deaths', 'es-aaa', 'ts', list(
-    #   model = 'AAA', lags = c(1,1,52),
-    #   persistence = NULL,
-    #   distribution = 'dnorm',
-    #   initial = 'optimal',
-    #   orders = list(ar = c(0), i = c(0), ma = c(0), select = FALSE)
-    # ),
-    
-    # 'Seasonal Arima (1,1,1)(1,1,1)52 deaths', 'sarima', 'ts', list(
-    #   model = 'NNN', lags = c(1,52),
-    #   persistence = NULL,
-    #   distribution = 'dnorm',
-    #   initial = 'backcasting',
-    #   orders = list(ar=c(1,1),i=c(1,1),ma=c(1,1))
-    # ),
-    
-    # ... INLA ------------------------------------------------------
-    
-    'LGMr', 'inla', list(
-      inla_formula = formula(
+    # Kontis style LGM without temperature effect
+    'LGMr', 'lgm', list(
+      formula = formula(
         death ~
           1 +
           global_slope +
@@ -167,15 +161,15 @@ mod_para <-
             model = 'seasonal', season.length = 52,
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           ) +
-          # independent remaining errors
           f(resid_iid,
             model = 'iid',
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           )
       )
     ),
-    'LGMrt', 'inla', list(
-      inla_formula = formula(
+    # Kontis style LGM with temperature effect
+    'LGMrt', 'lgm', list(
+      formula = formula(
         death ~
           1 +
           global_slope +
@@ -189,21 +183,19 @@ mod_para <-
             model = 'seasonal', season.length = 52,
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           ) +
-          # effect of temperature anomaly varies in a cyclic fashion
-          # over the week of a year
           f(week_rw, tanomaly,
             model = 'rw1', cyclic = TRUE,
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           ) +
-          # independent remaining errors
           f(resid_iid,
             model = 'iid',
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           )
       )
     ),
-    'LGMrt2', 'inla', list(
-      inla_formula = formula(
+    # Kontis style LGM with temperature effect and different order residuals
+    'LGMrt2', 'lgm', list(
+      formula = formula(
         death ~
           1 +
           global_slope +
@@ -229,7 +221,7 @@ mod_para <-
             hyper = list(prec = list(prior = 'loggamma', param = c(0.001, 0.001)))
           )
       )
-    )#,
+    )
     
   )
 
